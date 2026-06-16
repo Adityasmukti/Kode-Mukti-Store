434d7cb733b40acd3820b2af9fc64f409c00c4cc
Aditias Muktiyanto <Adityasmukti@users.noreply.github.com>
2026-06-16 18:16:54 +0700

    fix: sitemap URL uses https:// instead of http://

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,11 @@ Route::get('/robots.txt', function () {
 });
 
 Route::get('/sitemap.xml', function () {
+    $baseUrl = str_replace('http://', 'https://', url('/'));
     $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
     $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
     $xml .= '    <url>' . "\n";
-    $xml .= '        <loc>' . url('/') . '</loc>' . "\n";
+    $xml .= '        <loc>' . $baseUrl . '</loc>' . "\n";
     $xml .= '        <lastmod>' . date('Y-m-d') . '</lastmod>' . "\n";
     $xml .= '        <changefreq>weekly</changefreq>' . "\n";
     $xml .= '        <priority>1.0</priority>' . "\n";
